844584dbbe50a4981b42de4b573f374da1a1a449
Saheb Rahaman <premium47484@gmail.com>
2025-06-04 18:51:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "19259149"
 api_hash = "67e9ed15eb0adf9d3b391311933fa594"
-bot_token = "7561139321:AAHRcxgoJqlSq7I-1LNx-hmfO7S5TvFAx0A"
-auth_users = [6705657501]
+bot_token = ""
+auth_users = [6705657501 6559064772 7676466081]
